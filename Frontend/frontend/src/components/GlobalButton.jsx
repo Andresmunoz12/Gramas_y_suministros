3739dc1aa145b4c2d4b6f81c10933efb95723438
Andres <amunozlombana@gmail.com>
2026-06-27 13:48:26 -0500

    chore: remover carpeta Frontend de la rama Backend

--- Frontend/frontend/src/components/GlobalButton.jsx
+++
@@ -1,9 +0,0 @@
-import "../styles/GlobalButton.css";
-
-export default function GlobalButton({ children, onClick, className = "", ...props }) {
-  return (
-    <button className={`auth-button ${className}`} onClick={onClick} {...props}>
-      {children}
-    </button>
-  );
-}
